3d1ed4239353428af3fe6b331b9b0e2d61cd5a0e
Yanis Colin <yaniscol70@gmail.com>
2022-11-07 15:49:24 +0100

    commit3

--- PROJET/php-tableau/grille.php
+++ PROJET/php-tableau/grille.php
@@ -1,2 +1,7 @@
 <?php
-$largeur
+$largeur = readline("Saisir une Largeur : ");
+$hauteur = readline("Saisir une Hauteur : ");
+$grille = [];
+$grille = array_fill(0,$largeur,"-");
+
+print_r($grille);
